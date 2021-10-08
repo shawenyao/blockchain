8c5f275af7f4e60beaa432d28b37573103c3c4c2
Wenyao <38875149+shawenyao@users.noreply.github.com>
2021-10-07 23:09:39 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,10 @@
 import hashlib
 import json
-from textwrap import dedent
+# from textwrap import dedent
 from time import time
 from uuid import uuid4
-
 from flask import Flask
-
-
-class Blockchain(object):
-    ...
+from blockchain import Blockchain
 
 
 # Instantiate our Node
