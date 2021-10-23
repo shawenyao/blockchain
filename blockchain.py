17601b52d5fa58c01b921b1e79ccb77e0f313b6b
Wenyao <38875149+shawenyao@users.noreply.github.com>
2021-10-23 16:18:36 -0700

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -23,7 +23,7 @@ class Blockchain(object):
 
         # create the genesis block
         # where previous hash is hard-coded (64 characters to be consistent)
-        self.proof_of_work(previous_hash='not applicable for the genesis block                            ')
+        self.proof_of_work(previous_hash='[note:    previous hash is not applicable for the genesis block]')
 
     def new_block(self, nonce):
         """
